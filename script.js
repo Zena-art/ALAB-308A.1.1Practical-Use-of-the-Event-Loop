62f20dc0031a1f300f28f5ece213b9781b69833a
Zena-art <zena.nazim@gmail.com>
2024-10-05 17:34:50 -0500

    Task 2 is done

--- script.js
+++ script.js
@@ -16,3 +16,19 @@ try {
   console.log("number of recursive calls made: " + counter);
   console.log("Error details: ", error.message);
 }
+
+// Write a recursive function that completely flattens an array of nested arrays, regardless of how deeply nested the arrays are.
+// Once your recursive function is complete, trampoline it.
+
+const flattenArray = (arr) => {
+  return arr.reduce((acc, val) => {
+    if(Array.isArray(val)){
+      // If the value is an array, recursively flatten it
+      return acc.concat(flattenArray(val));
+    }else {
+      // If it's not an array, just add it to the accumulator
+      return acc.concat(val);
+    }
+  }, []);
+};
+
