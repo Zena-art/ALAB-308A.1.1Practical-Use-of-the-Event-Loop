df1fc9828493c6ef0ed12f07377013ad1fae5fba
Zena-art <zena.nazim@gmail.com>
2024-10-05 16:48:11 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// Declare a global counter variable.
+
+let counter = 0;
+
+// Create a simple function that increments the variable, and then calls itself recursively.
+function recursiveCalls(){
+  counter ++;
+  recursiveCalls();
+}
